34dbeded2beb2b1bc39b58ac6e0581adecb9d046
Joseph Casey <jcaseynz@gmail.com>
2018-04-29 20:41:30 +1200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardContainerComponent } from './dashboard/dashboard-container/dashboard-container.component';
+import { ProductContainerComponent } from './products/product-container/product-container.component';
+
+const appRoutes: Routes = [
+  { path: 'products', component: ProductContainerComponent },
+  { path: 'hero/:id',      component: DashboardContainerComponent },
+  {
+    path: 'dashboard',
+    component: DashboardContainerComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: DashboardContainerComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardContainerComponent,
+    ProductContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
